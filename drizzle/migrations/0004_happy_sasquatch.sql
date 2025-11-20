491d9747f5d1fc4826b21dc7d89f961a2136b6ee
HaexHub <mail@haex.space>
2025-11-20 01:21:42 +0100

    Fix migration: delete existing vault keys before adding encrypted vault name columns

--- drizzle/migrations/0004_happy_sasquatch.sql
+++ drizzle/migrations/0004_happy_sasquatch.sql
@@ -1,3 +1,5 @@
+-- Delete all existing vault keys (they need to be recreated with encrypted vault names)
+DELETE FROM "vault_keys";--> statement-breakpoint
 ALTER TABLE "vault_keys" RENAME COLUMN "nonce" TO "vault_key_nonce";--> statement-breakpoint
 ALTER TABLE "vault_keys" ADD COLUMN "encrypted_vault_name" text NOT NULL;--> statement-breakpoint
 ALTER TABLE "vault_keys" ADD COLUMN "vault_name_nonce" text NOT NULL;
